ad956095efdc091f54be6e2b360a6d49df6968c1
Wladislaw Imgrunt <w.imgrunt.extern@senec.com>
2024-12-06 16:55:31 +0100

    Initial Commit

---
+++ ErgWallet/ErgWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErgWalletApp.swift
+//  ErgWallet
+//
+//  Created by Wladislaw Imgrunt on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ErgWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
